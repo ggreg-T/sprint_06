3bcf4bce32ebde27833b8a609518130161263a6e
gregory <graigtech@gmail.com>
2022-03-31 11:07:56 +0400

    update

---
+++ README.txt
@@ -0,0 +1,6 @@
+﻿# php CRUD Vap Factory
+
+Un site de gestion des stocks de cigarettes électroniques et des différents liquides.
+ Insérer, lire , modifier et supprimer des informations avec PHP et MySQL
+
+Lancer avec index.php
--- config.php
+++ config.php
@@ -1,15 +1,18 @@
 <?php
-/* Database connexion */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'vap_factory');
- 
-/* Connexion à la base de données */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// verifier connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+try {
+    // dev configuration
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
+        $bdd = new mysqli(
+            'localhost',
+            'root',
+            '',
+            'vap_factory'
+        );
+    }
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
+        $bdd = new mysqli('109.234.164.161', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'], 'sc1lgvu9627_techer-gregory.sprint-06 ');
+    }
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
 }
 ?>
